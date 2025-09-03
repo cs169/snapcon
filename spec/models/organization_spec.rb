8080e0174d86759c2204ffd409634881182b2420
Michael Ball <michael@mball.co>
2025-09-03 16:57:20 +0200

    Disable organization spec since those will go away soon

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -16,12 +16,12 @@ describe Organization do
   let(:organization) { create(:organization) }
 
   describe 'validation' do
-    it 'is not valid without a name' do
+    xit 'is not valid without a name' do
       expect(subject).to validate_presence_of(:name)
     end
   end
 
   describe 'associations' do
-    it { is_expected.to have_many(:conferences).dependent(:destroy) }
+    xit { is_expected.to have_many(:conferences).dependent(:destroy) }
   end
 end
