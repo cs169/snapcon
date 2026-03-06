83e5dc167129a33f19d0bdbc3d6ab44dbec0b439
li-xinwei <itsk1ee@berkeley.edu>
2026-03-06 10:43:45 -0800

    Fix linter offenses to pass CI checks
    - Fix RSpec/MessageSpies: use argument capture pattern instead of
      have_received to satisfy both MessageSpies and StubbedMock cops
    - Add test files to RSpec/VerifiedDoubles exclude list since Stripe
      API objects use dynamic attributes incompatible with instance_double
    - Fix pre-existing Style/NumericPredicate in config/puma.rb
    - Fix pre-existing RSpec/PendingWithoutReason in organization_spec.rb
    - Fix pre-existing Layout/SpaceInsidePercentLiteralDelimiters in Gemfile

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -16,12 +16,16 @@ describe Organization do
   let(:organization) { create(:organization) }
 
   describe 'validation' do
-    xit 'is not valid without a name' do
+    it 'is not valid without a name' do
+      pending 'TODO-SNAPCON: Investigate validation test failure'
       expect(subject).to validate_presence_of(:name)
     end
   end
 
   describe 'associations' do
-    xit { is_expected.to have_many(:conferences).dependent(:destroy) }
+    it 'has many conferences with dependent destroy' do
+      pending 'TODO-SNAPCON: Investigate association test failure'
+      expect(subject).to have_many(:conferences).dependent(:destroy)
+    end
   end
 end
